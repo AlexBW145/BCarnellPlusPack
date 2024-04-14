91b9e89da83a33ad545aacd831d04fc01062091c
AlexBW145 <92541723+AlexBW145@users.noreply.github.com>
2024-04-14 18:57:31 -0500

    Likely missed that

--- BCarnellEditor/obj/Debug/netstandard2.0/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace BCarnellEditor
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "BCarnellEditor";
-        public const string PLUGIN_NAME = "BCarnellEditor";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
